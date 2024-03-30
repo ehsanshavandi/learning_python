d1e000f4f4c1b4942a4beba863800c12dc91ffb1
ehsanshavandi <ehsan.shavandi@gmail.com>
2024-03-30 18:07:39 +0330

    first commit - class variable, instance variable and methods - getters and setters

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+
+
+class Student:
+    """Class's variables"""
+    rooms = ["Gryffindor", "Hufflepuff", "Ravenclaw"]
+
+    def __init__(self, name, house):
+        """Go through the setters"""
+        """Instance's variables"""
+        # This is a convention: To have difference between name and name function we need to use _name
+        self.name = name
+        self.house = house
+
+    @property
+    def name(self):
+        """dot operator without assignment"""
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        """dot operator without assignment"""
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        self._house = house
+
+    def __str__(self):
+        return f"{self.name}, {self.house}"
+
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.rooms))
+
+    @classmethod
+    def get(cls):
+        """Factory Method"""
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+
+
+ehsan = Student("Ehsan", "Arash street")
+user = Student.get()
+Student.sort("Ehsan")
+print(type(ehsan))
